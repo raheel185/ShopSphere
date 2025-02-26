039dfd9eb6b99ca88fa19f157dd3f511fc531096
Seth Ciasulli <diego.mandu.deve@gmail.com>
2025-02-26 12:42:29 +0500

    Front-end files

---
+++ front-end/src/components/Nav.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
+
+const Nav = () => {
+  return (
+    <div className='flex items-center justify-between py-5 font-medium'>
+      
+      <img src={assets.logo} className='w-36' alt="logo" />
+        <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
+            <NavLink to="/" className="flex flex-col items-center gap-1">
+                <p>HOME</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink to="/collection" className="flex flex-col items-center gap-1">
+                <p>COLLECTION</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink to="/about" className="flex flex-col items-center gap-1">
+                <p>ABOUT</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+            <NavLink to="/contact" className="flex flex-col items-center gap-1">
+                <p>CONTACT</p>
+                <hr className='w-2/4 border-none h-[1.5px] bg-gray-700 hidden'/>
+            </NavLink>
+        </ul>
+
+        <div className='flex items-center gap-6'>
+            <img src={assets.search_icon} alt="" className='w-5 cursor-pointer' />
+            <div className='group relative'>
+                <img className='w-5 cursor-pointer' src={assets.profile_icon} alt=""/>
+                <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                    <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Nav
