1effdb16e86abac39719016b2a9167fb3bba764b
Raheel Fida <diego.mandu.deve@gmail.com>
2025-02-26 13:05:14 +0500

    Nav-update

--- front-end/src/components/Nav.jsx
+++ front-end/src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 const Nav = () => {
   return (
@@ -31,10 +31,16 @@ const Nav = () => {
                 <img className='w-5 cursor-pointer' src={assets.profile_icon} alt=""/>
                 <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                     <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
-                        
+                        <p className='cursor-pointer hover:text-black'>My Profile</p>
+                        <p className='cursor-pointer hover:text-black'>Order</p>
+                        <p className='cursor-pointer hover:text-black'>Logout</p>
                     </div>
                 </div>
             </div>
+            <Link to="/cart" className='relative'>
+                <img src={assets.cart_icon} className='w-5 min-w-5' alt="" />
+                <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>2</p>
+            </Link>
         </div>
     </div>
   )
