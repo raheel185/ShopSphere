ca31077279618e5774f26d9cd43f894e01e550e3
Raheel Fida <diego.mandu.deve@gmail.com>
2025-02-27 15:37:00 +0500

    Update

--- front-end/src/pages/Collection.jsx
+++ front-end/src/pages/Collection.jsx
@@ -1,19 +1,21 @@
 import React, { useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import { assets } from '../assets/assets'
+import Title from '../components/Title'
 
 const Collection = () => {
 
   const { products } = useState(ShopContext)
   const [showFilter, setShowfilter] = useState(false)
- 
+
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
       
       <div className='min-w-60'>
-        <p className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
+        <p onClick={()=>setShowfilter(prev=>!prev)} className='my-2 text-xl flex items-center cursor-pointer gap-2'>FILTERS
           <img className={`h-3 sm:hidden ${showFilter ? 'rotate-90' : ''}`} src={assets.dropdown_icon} />
         </p>
+
         {/* Category filter */}
         <div className={`border border-gray-300 pl-5 py-3 mt-6 ${showFilter ? '' : 'hidden'}`}>
            <p className='mb-3 text-sm font-medium'>CATEGORIES</p>
@@ -47,6 +49,16 @@ const Collection = () => {
         </div>
         
       </div>
+
+      {/* Right Side Section */}
+      <div className='flex-1'>
+
+        <div className='flex justify-between text-base sm:text2xl mb-4'>
+            <Title text1={'ALL'} text2={'COLLECTIONS'} />
+        </div>
+
+      </div>
+
 
     </div>
   )
