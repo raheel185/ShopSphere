a7034d49232450003a4a2ac6d760a3309f2bf8ff
Raheel Fida <diego.mandu.deve@gmail.com>
2025-03-01 12:39:54 +0500

    Updates

--- front-end/src/pages/Product.jsx
+++ front-end/src/pages/Product.jsx
@@ -7,7 +7,7 @@ const Product = () => {
   const {productId} = useParams()
   const {products} = useContext(ShopContext)
 
-  const [product, setProduct] = useState(false)
+  const [productdata, setProduct] = useState(false)
   const [image, setImage] = useState(false)
 
   const fetchProduct = async()=>{
@@ -20,14 +20,6 @@ const Product = () => {
       }
      
     })
-   
-    // products.map((item)=>{
-    //   if(item._id == productId){
-    //     setProduct(item)
-    //     console.log(item)
-    //     return null
-    //   }
-    // })
 
   }
 
@@ -37,11 +29,28 @@ const Product = () => {
 
   },[productId, products])
 
-  return (
-    <div>
-      {productId}
+  return productdata ? (
+    <div className='border-t-2 pt-10 transition-opacity ease-in duration-500 opacity-100'>
+
+        <div className='flex gap-12 sm:gap-12 flex-col sm:flex-row'>
+
+            <div className='flex-1 flex flex-col-reverse gap-3 sm:flex-row'>
+                <div className='flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full'>
+                        {
+                          productdata.image.map((item,index)=>(
+                            <img onClick={()=>setImage(item)} src={item} key={index} alt="" className='w-[24%] sm:w-full sm:mb-3 flex-shrink-0 cursor-pointer' />
+                          ))
+                        }
+                </div>
+                <div className='w-full sm:w-[80%]'>
+                        <img src={image} className='w-full h-auto' alt="" />
+                </div>
+            </div>
+
+        </div>
+
     </div>
-  )
+  ): <div className='opacity-0'></div>
 }
 
 export default Product
