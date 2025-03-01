01909dc758c1fde76c05a378b8497586d7e575e3
Raheel Fida <diego.mandu.deve@gmail.com>
2025-03-01 19:38:34 +0500

    Update

--- front-end/src/pages/Product.jsx
+++ front-end/src/pages/Product.jsx
@@ -10,6 +10,7 @@ const Product = () => {
 
   const [productdata, setProduct] = useState(false)
   const [image, setImage] = useState(false)
+  const [size, setSize] = useState('')
 
   const fetchProduct = async()=>{
 
@@ -61,10 +62,39 @@ const Product = () => {
                         <p className='pl-2'>(122)</p>
                   </div>
                   <p className='mt-5 text-3xl font-medium'>{currency}{productdata.price}</p>
-                  <p>{productdata.description}</p>
+                  <p className='mt-5 text-gray-500 md:w-4/5'>{productdata.description}</p>
+                  <div className='flex flex-col gap-4 my-8'>
+                        <p>Select Size</p>
+                        <div className='flex gap-2'>
+                            {
+                              productdata.sizes.map((item,index)=>(
+                                <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 cursor-pointer ${item == size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                              ))
+                            }
+                        </div>
+                  </div>
+                  <button className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+                  <hr className='mt-8 sm:w-4/5' />
+                  <div className='text-sm text-gray-500 mt-5 flex flex-col gap-1'>
+                            <p>100% Original product.</p>
+                            <p>Cash on delivery is available on this product.</p>
+                            <p>Easy return and exchange policy within 7 days.</p>
+                  </div>
             </div>
-
         </div>
+
+        {/* Description and Review */}
+        <div className='mt-20'>
+            <div className='flex'>
+              <b className='border px-5 py-3 text-sm'>Description</b>
+              <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+            </div>
+            <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus eius dolor delectus similique soluta consequuntur laudantium nisi placeat ducimus itaque?</p>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Natus, odio ipsa? Quia sequi quae corporis? Earum ea reiciendis nulla blanditiis molestiae numquam quod. Delectus, ab?</p>
+            </div>
+        </div>
+
 
     </div>
   ): <div className='opacity-0'></div>
