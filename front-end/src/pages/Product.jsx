4b4f71613914215eccc264674f313497405fd962
Raheel Fida <diego.mandu.deve@gmail.com>
2025-03-01 11:51:29 +0500

    Single product

--- front-end/src/pages/Product.jsx
+++ front-end/src/pages/Product.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext'
 
 const Product = () => {
 
   const {productId} = useParams()
+  const {products} = useContext(ShopContext)
+
+  const [product, setProduct] = useState(false)
+  const [image, setImage] = useState(false)
+
+  const fetchProduct = async()=>{
+
+    products.filter((item)=>{
+      if(item._id == productId){
+        setProduct(item)
+        setImage(item.image[0])
+        return null
+      }
+     
+    })
+   
+    // products.map((item)=>{
+    //   if(item._id == productId){
+    //     setProduct(item)
+    //     console.log(item)
+    //     return null
+    //   }
+    // })
+
+  }
+
+  useEffect(()=>{
+
+    fetchProduct()
+
+  },[productId, products])
 
   return (
     <div>
