795249d1792d4836decc60c3818fed2cbe8918f8
Raheel Fida <diego.mandu.deve@gmail.com>
2025-03-02 12:38:18 +0500

    Update

--- front-end/src/context/ShopContext.jsx
+++ front-end/src/context/ShopContext.jsx
@@ -11,6 +11,14 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState('')
     const [showSearch, setShowSearch] = useState(false)
 
+    const [cartItems, setCartItems] = useState({})
+
+    const addToCart = async (itemId, size) => {
+
+        let cartData = structuredClone(cartItems)
+
+    }
+
     const value = { products, currency, delivery_fee, search, showSearch, setSearch, setShowSearch }
 
     return(
