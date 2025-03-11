a2c8e6e7886f10c53aa20342a75344144c91d953
Raheel Fida <diego.mandu.deve@gmail.com>
2025-03-11 10:53:55 +0500

    Update

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import axios from 'axios'
 import {toast} from 'react-toastify'
@@ -45,6 +45,14 @@ const Login = () => {
       }
 
   }
+
+  useEffect(()=>{
+
+      if(token){
+        navigate("/")
+      }
+
+  },[token])
 
   return (
     <form onSubmit={onSubmitHandler} className='flex flex-col items-center w-[90%] sm:max-w-96 m-auto mt-14 gap-4 text-gray-800'>
