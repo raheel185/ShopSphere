1b413c3d08588b9dd77ea47c499a28f97ec6a490
Raheel Fida <diego.mandu.deve@gmail.com>
2025-03-11 08:31:22 +0500

    Update

--- front-end/src/hooks/fetchProducts.jsx
+++ front-end/src/hooks/fetchProducts.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import axios from 'axios'
+import { ShopContext } from '../context/ShopContext'
+
 
 const fetchProducts = () => {
+
+    const {backendUrl} = useContext(ShopContext)
 
     const [products, setproducts] = useState('Lorem Ipsum Dolor')
 
     async function getData(){
         try {
-            
+            const response = await axios.get(`${backendUrl}/api/product/list`)
         } catch (error) {
             
         }
